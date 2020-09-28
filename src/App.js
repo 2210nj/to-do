46ff0322871d8f588c74634b452e94937d9656ae
Namita Jadhav <2210nj@gmail.com>
2020-09-28 13:46:39 +0530

    Add a basic form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// import Button from '@material-ui/core/Button';
+import { FormControl, Button, InputLabel, Input } from '@material-ui/core';
 
 function App() {
+  const [todos, setTodos] = useState(['Study', 'Work']);
+  const [input, setInput] = useState('')
+
+  const addToDo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World  </h1>
+      <FormControl>
+        <InputLabel>✅ Write a to-do</InputLabel>
+        <Input value={input} onChange={event => (setInput(event.target.value))} />
+        <Button type="submit" disabled={!input}  onClick={addToDo} variant="contained" color="primary">
+          Add
+        </Button>
+      </FormControl>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
