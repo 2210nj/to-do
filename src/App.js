e76aed041df2f9abac79543c3e444d5d85e1acef
Namita Jadhav <2210nj@gmail.com>
2020-10-02 06:40:18 +0530

    Update css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Todo from "./Todo";
 import db from "./firebase";
 import firebase from "firebase";
 import CompletedTodo from "./CompletedTodo";
+import { Image } from "@material-ui/icons";
+import image from "./images/desk-image.png";
+import MenuIcon from "@material-ui/icons/Menu";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { IconButton } from "@material-ui/core";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -17,11 +23,12 @@ function App() {
       .orderBy("timeStamp", "desc")
       .onSnapshot((snapshot) => {
         console.log(snapshot.docs.map((doc) => doc.data()));
-        setTodos(snapshot.docs.map((doc) => ({id: doc.id ,todo: doc.data().todo})));
+        setTodos(
+          snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo }))
+        );
       });
 
-
-      db.collection("completedTodos")
+    db.collection("completedTodos")
       .orderBy("timeStamp", "desc")
       .onSnapshot((snapshot) => {
         console.log(snapshot);
@@ -39,10 +46,43 @@ function App() {
     setTodos([...todos, input]);
     setInput("");
   };
+
+  const onMenuClick = () => {
+    console.log("You just clicked on Menu icon");
+  };
+
+  const onAddClick = () => {
+    console.log("You just clicked on Add icon");
+  };
+
+  const onMoreOptionsClick = () => {
+    console.log("You just clicked on More Options icon");
+  };
   return (
-    <div className="App">
-      <h1>My Tasks</h1>
-      <form>
+    <div className="app">
+      <div className="app__container">
+        <div className="app__header">
+          <h2>My Tasks</h2>
+        </div>
+        <div className="app__body">
+          <img className="app__image" src={image} />
+          <p>Let's get some work done..</p>
+          <p>Anything to add?</p>
+        </div>
+        <div className="app__footer">
+          <IconButton onClick={onMenuClick}>
+            <MenuIcon />
+          </IconButton>
+          <IconButton>
+            <AddCircleIcon onClick={onAddClick} />
+          </IconButton>
+          <IconButton onClick={onMoreOptionsClick}>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      {/* <form>
         <FormControl>
           <InputLabel>✅ Write a to-do</InputLabel>
           <Input
@@ -68,11 +108,11 @@ function App() {
         ))}
       </ul>
       <p>My completed todos</p>
-      <ul>
+      <ul> 
         {completedTodos.map((completedTodo) => (
           <CompletedTodo todo={completedTodo} />
         ))}
-      </ul>
+      </ul>*/}
     </div>
   );
 }
